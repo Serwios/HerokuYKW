62d843715f27f2c539bb448b4d71688a9fff5274
Serwio YK <56473255+Serwios@users.noreply.github.com>
2021-06-26 16:51:10 +0300

    Update SpringLoginFormAndOauth2TutorialApplication.java

--- src/main/java/com/dzeru/springloginformandoauth2tutorial/SpringLoginFormAndOauth2TutorialApplication.java
+++ src/main/java/com/dzeru/springloginformandoauth2tutorial/SpringLoginFormAndOauth2TutorialApplication.java
@@ -1,11 +1,11 @@
-package com.dzeru.springloginformandoauth2tutorial;
+package com.dzeru.YKWorkshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringLoginFormAndOauth2TutorialApplication {
+public class YKWorkshop {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringLoginFormAndOauth2TutorialApplication.class, args);
+		SpringApplication.run(YKWorkshop.class, args);
 	}
 }
